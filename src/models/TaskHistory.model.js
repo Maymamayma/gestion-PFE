d5ed836dd31ecf006c4b5f315e92c233bc788a6e
Ala Eddine Guidara <alaguidara23@gmail.com>
2025-11-27 02:24:20 +0100

    feat(task): add status change with automatic history tracking

---
+++ src/models/TaskHistory.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const TaskHistorySchema = new mongoose.Schema({
+  taskId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Task",
+    required: true
+  },
+  oldStatus: {
+    type: String,
+    enum: ["ToDo", "InProgress", "Standby", "Done"],
+    required: true
+  },
+  newStatus: {
+    type: String,
+    enum: ["ToDo", "InProgress", "Standby", "Done"],
+    required: true
+  },
+  changedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  notes: {
+    type: String
+  },
+  changedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("TaskHistory", TaskHistorySchema);
