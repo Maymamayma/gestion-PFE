9b8b453b317092d53eb9bdaf422fd1ec60c845b6
Maymamayma <119966347+Maymamayma@users.noreply.github.com>
2025-12-01 22:26:34 +0100

    fix projectReport route structure

--- src/routes/projectReport.routes.js
+++ src/routes/projectReport.routes.js
@@ -1,8 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const { generateProjectReport } = require("../controllers/projectReport.controller");
+import express from "express";
+import * as generateProjectReport from "../controllers/projectReport.controller.js";
+
+export const router = express.Router();
+
+router.get("/projects/:projectId/report", generateProjectReport);
+//const express = require("express");
+//const router = express.Router();
+//const { generateProjectReport } = require("../controllers/projectReport.controller");
 
 // Générer rapport HTML global pour un projet
-router.get("/projects/:projectId/report", generateProjectReport);
+//router.get("/projects/:projectId/report", generateProjectReport);
 
-module.exports = router;
+//module.exports = router;
