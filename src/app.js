bfc9aa7dfcc51f3f26d16f60debfaf5dc63d22b7
Aymen-Settey <messisettey@gmail.com>
2026-03-26 16:45:44 +0100

    meetings fixes

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api/project-reports", projectReportRouter);
-// app.use("/api/reports", reportRouter);
+app.use("/api/reports", reportRouter);
 // app.use("/api/report-histories", reportHistoryRouter);
 app.use("/api/sprint-reports", sprintReportRouter);
 app.use("/api/tasks", taskRouter);
